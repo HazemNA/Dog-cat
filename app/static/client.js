a16cf6c46293339017527bbba74cf277bd80a33f
HazemNA <65853663+HazemNA@users.noreply.github.com>
2020-11-27 23:41:01 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -2,10 +2,12 @@ var el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
+  el("extradata-label").innerHTML = `showPicker`;
 }
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
+  el("extradata-label").innerHTML = `showPicked`;
   var reader = new FileReader();
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
@@ -16,11 +18,16 @@ function showPicked(input) {
 }
 
 function analyze() {
+  el("extradata-label").innerHTML = `analyze`;
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
-
+  
   el("analyze-button").innerHTML = "Analysis in Progress...";
   el("result-label").innerHTML = `  `;
+  
+  // resize image
+  ResizeImage()
+  
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -41,3 +48,54 @@ function analyze() {
   xhr.send(fileData);
 }
 
+function ResizeImage() {
+    el("extradata-label").innerHTML = `ResizeImage`;
+    // next 2 lines to take the uploaded filename
+    var filesToUpload = el("file-input").files;
+    var file = filesToUpload[0];
+  
+    // Original
+    //var filesToUpload = document.getElementById('imageFile').files;
+    //var file = filesToUpload[0];
+  
+    // Create an image
+    var img = document.createElement("img");
+    // Create a file reader
+    var reader = new FileReader();
+    // Set the image once loaded into file reader
+    reader.onload = function(e) {
+            img.src = e.target.result;
+
+            var canvas = document.createElement("canvas");
+            //var canvas = $("<canvas>", {"id":"testing"})[0];
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+
+            // maximum size
+            var MAX_WIDTH = 200;
+            var MAX_HEIGHT = 200;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+
+            var dataurl = canvas.toDataURL("image/png");
+            document.getElementById('output').src = dataurl;
+        }
+        // Load files into file reader
+    reader.readAsDataURL(file);
+}
